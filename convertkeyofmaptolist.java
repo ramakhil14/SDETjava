953a4f4180072c69776814918ee9af65c329b182
ramakhil14 <91665963+ramakhil14@users.noreply.github.com>
2021-09-30 17:41:42 +0530

    Update convertkeyofmaptolist.java

--- convertkeyofmaptolist.java
+++ convertkeyofmaptolist.java
@@ -3,17 +3,15 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.HashSet;
-import java.util.Set;
+
 public class convertkeyofmaptolist {
 	 public static void main(String[] args) {
 
 		 Map<Integer, Integer> map = new HashMap<Integer, Integer>(); 
 		 
-		 Set<Integer> keyset = new HashSet<Integer>();
 		 List<Integer> arraylist = new ArrayList<Integer>();
 		 
-		 arraylist.addAll(keyset);
+		 arraylist.addAll(map.keySet());
 		 System.out.println("list is");
 		 for(int i: arraylist) {
 		 System.out.println(i);
